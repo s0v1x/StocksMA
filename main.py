139b666d09110d5a15c8d7723cbff7bfa173b438
sovix <mac@sovix.local>
2022-03-07 13:04:34 +0100

    get_data func

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup 
 import pandas as pd
 from io import StringIO
+from datetime import datetime, date
+from dateutil.relativedelta import relativedelta
 
-r = requests.get('https://www.marketwatch.com/investing/stock/dwy/download-data?countrycode=ma')
-soup = BeautifulSoup(r.content, "html.parser")
+def get_data(ticker, start_date, end_date):
 
+  today = datetime.now()
+  one_year_from_now = today - relativedelta(years=1)
 
+  if datetime.strptime(end_date, "%m/%d/%Y") > today:
+    raise ValueError("end_date is greater than {today}".format(today=today.strftime('%m/%d/%Y')))
+  if datetime.strptime(start_date, "%m/%d/%Y") < one_year_from_now:
+    raise ValueError("start_date is limited to a maximum of one year")
 
-data = soup.find("a", class_="link link--csv m100")['href']
-rr = requests.get(data)
+  url = 'https://www.marketwatch.com/investing/stock/{ticker}/download-data?startDate={s_date}&endDate={e_date}&countryCode=ma'.format(ticker=ticker,
+                                                                                                                                       s_date=start_date,
+                                                                                                                                       e_date=end_date)
+  
+  r = requests.get(url)
+  soup = BeautifulSoup(r.content, "html.parser")
+  url_data = soup.find("a", class_="link link--csv m100")['href']
+  data = requests.get(url_data)
+  df = pd.read_csv(StringIO(data.text), sep=",", index_col=[0])
+  df = df[end_date : start_date]
+  for cl in df.columns:
+    df[cl] = df[cl].apply(convert_obj)
 
-print(pd.read_csv(StringIO(rr.text), sep=","))
+  return df
