65d779a08cd8b785bfba0595886d4418b8a5da3c
Aymane Boumaaza <aymaneboumaaza@gmail.com>
2022-03-18 01:44:03 +0100

    Update tests (WIP :construction:)

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,47 +1,21 @@
-import pandas as pd
+from requests.models import Response
 
 import src.StocksMA as Stocks
+import src.utils as utils
 
 
-def test_get_isin(example_company: str) -> None:
-    assert Stocks.get_isin(example_company) == ('Maroc Telecom', 'MA0000011488')
+def mock_get_isin_request(*args, **kwargs) -> Response:
+    response = Response()
+    response.status_code = 200
+    response._content = b'{"result": [{"name": "Maroc Telecom", "isin": "US0378331005"}]}'
+    return response
+
+
+def test_get_isin(monkeypatch) -> None:
+    monkeypatch.setattr(utils, 'get_request', mock_get_isin_request)
+    assert isinstance(Stocks.get_isin("IAM"), tuple)
 
 
 def test_get_market_status() -> None:
     stat = Stocks.get_market_status()
     assert isinstance(stat, str)
-
-
-def test_types(example_company: str) -> None:
-    type1 = Stocks.get_data_stock(example_company, '2021-02-13', '2022-02-07')
-    assert isinstance(type1, pd.DataFrame)
-
-    type2 = Stocks.get_data(example_company, '2021-02-13')
-    assert isinstance(type2, pd.DataFrame)
-
-    type3 = Stocks.get_quick_info(example_company)
-    assert isinstance(type3, pd.DataFrame)
-
-    type4 = Stocks.get_data_intraday(example_company)
-    assert isinstance(type4, pd.DataFrame)
-
-    type5 = Stocks.get_ask_bid(example_company)
-    assert isinstance(type5, pd.DataFrame)
-
-    type6 = Stocks.get_balance_sheet(example_company)
-    assert isinstance(type6, pd.DataFrame)
-
-    type7 = Stocks.get_income_statement(example_company)
-    assert isinstance(type7, pd.DataFrame)
-
-    type8 = Stocks.get_cash_flow(example_company)
-    assert isinstance(type8, pd.DataFrame)
-
-    type9 = Stocks.get_quote_table(example_company)
-    assert isinstance(type9, pd.DataFrame)
-
-    type10 = Stocks.get_company_officers(example_company)
-    assert isinstance(type10, pd.DataFrame)
-
-    type11 = Stocks.get_company_info(example_company)
-    assert isinstance(type11, pd.DataFrame)
